f9561b61dd3a47d6ed6fe4b65df6a0ae57492349
hmadjarian2 <hmadjarian@rogers.com>
2010-04-04 10:54:57 -0400

    build message

--- app/controllers/mythtv_controller.rb
+++ app/controllers/mythtv_controller.rb
@@ -1,14 +1,28 @@
 require 'socket'
 class MythtvController < ApplicationController
   def test
-    server = TCPSocket::new("192.168.1.10", 6543)
-    server.write("21      MYTH_PROTO_VERSION 50")
-    str = server.recv(22)
-    server.write("23      ANN Playback sycamore 0")
-    str = str + "\n" + server.recv(11)
-    server.write("23      QUERY_RECORDINGS Delete")
-    str = str + server.recv(8)
-    server.close
+    connection = TCPSocket::new("192.168.1.10", 6543)
+    connection.write(buildMessage("MYTH_PROTO_VERSION 50")
+    str = connection.recv(22)
+    connection.write(buildMessage("ANN Playback sycamore 0")
+    str = str + "\n" + connection.recv(11)
+    connection.write(buildMessage("QUERY_RECORDINGS Delete")
+    retMsg = connection.recv(8)
+    str = str + retMsg
+    n = Integer(retMsg)
+    i = 0
+    reply = ""
+    while i < n:
+      reply += connection.recv(n - i)
+      i = reply.length
+    end
+
+    str += reply
+    connection.close
     render :text => str
+  end
+
+  def buildMessage(command)
+    return "%-8d%s" % [message.length, message]
   end
 end
